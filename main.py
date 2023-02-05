2d4ccc0b0c05d9cc873dad4acbfea7ef50fb3308
Ricardo da Rocha <ricardodarocha@outlook.com>
2023-02-04 23:51:33 -0300

    Initial Container

--- main.py
+++ main.py
@@ -1,28 +1,85 @@
-import flet as ft
+import flet
+from flet import *
+from functools  import partial
+import time
 
-def main(page): 
-    def on_keyboard(e: ft.KeyboardEvent):
-        page.add(
-            ft.Text(
-                f"Key: {e.key}, Shift: {e.shift}, Control: {e.ctrl}, Alt: {e.alt}, Meta: {e.meta}"
+# Sidebar class
+class ModernNavBar(UserControl):
+    def __init__(self):
+        super().__init__()
+
+    def UserData(self, initials: str, name: str, description: str):
+        return Container(
+            bgcolor="bluegrey900",
+            content=Row(
+                controls=[
+                    Container(
+                        width=42,
+                        height=42,
+                        bgcolor="#37474F",
+                        alignment=alignment.center,
+                        border_radius=8,
+                        content=Text(
+                            value=initials,
+                            size=20,
+                            weight="bold",
+                            color="white54"
+                        ),
+                    ),
+                Column(
+                    spacing=1,
+                    alignment='center',
+                    controls=[
+                        Text(
+                            value=name, 
+                            size=12, 
+                            weight="bold",
+                            color="white54"
+                            ),
+                        Text(
+                            value=description,  
+                            size=9, 
+                            weight="w400",
+                            color="white54"
+                            ),
+                        ],
+                    opacity=1,
+                    animate_opacity=200,
+
+                ),
+                ]
             )
         )
-
-    page.on_keyboard_event = on_keyboard
+        
+    def build(self):
+        return Container(
+            width=200,
+            height=580,
+            padding=padding.only(top=10),
+            alignment=alignment.center,
+            content=Column(controls=[
+            self.UserData("Ri", "Ricardo da Rocha", "designer"),
+            ]))
+    
+def main(page: Page):
+    page.title = "Ricardo da Rocha inc"
+    page.horizontal_alignment='center'
+    page.vertical_alignment='center'
+    page.bgcolor='#212121'
     page.add(
-        ft.Text("Pressione qualquer tecla com CTRL, ALT, SHIFT ...")
+        Container(
+            width=300,    
+            height=600,    
+            bgcolor="#263238",
+            border_radius=10,
+            alignment=alignment.center,
+            animate=animation.Animation(500, 'decelerate'),
+            padding=10,
+            content=ModernNavBar()
+        )
     )
-    def btn_click(e):
-        if not txt_name.value:
-            txt_name.error_text = "Qual o seu nome?"
-            page.update()
-        else:
-            name = txt_name.value
-            page.clean()
-            page.add(ft.Text(f"Hello, {name}!"))
 
-    txt_name = ft.TextField(label="Your name")
+    page.update()
 
-    page.add(txt_name, ft.ElevatedButton("Say hello!", on_click=btn_click))
-
-ft.app(target=main)
+if __name__ == '__main__':
+    flet.app(target=main)
